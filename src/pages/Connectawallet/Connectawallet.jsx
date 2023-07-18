c8e350b21419bd2622c155ae30b4a9409e179046
sonhagit <havansoncao2@gmail.com>
2023-07-18 14:59:28 +0700

    sửa lỗi

--- src/pages/Connectawallet/Connectawallet.jsx
+++ src/pages/Connectawallet/Connectawallet.jsx
@@ -1,10 +1,7 @@
 import { useEffect } from 'react'
 import '../Connectawallet/Connectawallet.css'
-import Coin from "../../assets/Wallet/Coinbase.svg"
-import Metamask from "../../assets/Wallet/Metamask.svg"
-import WalletConnect from "../../assets/Wallet/WalletConnect.svg"
 
-    
+
 const Connectawallet = () => {
   return (
     <div>
@@ -18,11 +15,11 @@ const Connectawallet = () => {
                 <p>Choose a wallet you want to connect. There are several wallet providers.</p>
                 </div>
             <div className='Button'>
-                <button> <img src={Metamask} alt="" />
+                <button> <img src={"https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/metamask@2x.svg"} alt="" />
                 <p>Metamask</p>
                 </button>
-                <button> <img src={WalletConnect} alt="" />WalletConnect</button>
-                <button> <img src={Coin} alt="" />Coinbase</button>
+                <button> <img src={"https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/walletconnect@2x.svg"} alt="" />WalletConnect</button>
+                <button> <img src={"https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/coinbase@2x.svg"} alt="" />Coinbase</button>
             </div>
             </div>
         </div>
